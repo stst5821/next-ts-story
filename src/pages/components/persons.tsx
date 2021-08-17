f4b55b1d20c47e35a72f495da46b7b4036a02e9f
stst5821 <stst5821@gmail.com>
2021-08-17 15:15:20 +0900

    配列、sort実装

--- src/pages/components/persons.tsx
+++ src/pages/components/persons.tsx
@@ -1,20 +1,45 @@
 import { FC } from "react";
 
-type Props = {
+type members = {
   name: string;
   tel: string;
   id: number;
 };
 
-export const Persons: FC<Props> = (props) => {
+const members = [
+  { name: "としみつ", tel: "090-0000-0000", id: 3 },
+  { name: "rika", tel: "090-0000-0010", id: 5 },
+  { name: "まさき", tel: "090-0110-0010", id: 8 },
+  { name: "yoshiaki", tel: "090-0010-0010", id: 1 },
+  { name: "いっち", tel: "090-0010-0011", id: 2 },
+];
+
+members.sort(function (a, b) {
+  if (a.id > b.id) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+export const Persons: FC<members> = () => {
   return (
     <>
       <ul>
-        <li>名前:{props.name}</li>
-        <li>電話番号:{props.tel}</li>
-        <li>ID:{props.id}</li>
+        {members.map((member) => (
+          <div key={member.id}>
+            <li>
+              名前:{member.name}
+              <br />
+              電話番号:{member.tel}
+              <br />
+              ID:{member.id}
+            </li>
+          </div>
+        ))}
       </ul>
     </>
   );
 };
+
 export default Persons;
