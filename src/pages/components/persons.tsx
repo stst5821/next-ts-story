93fe987c8f94a8499162faeabf29435abf7ef8bd
stst5821 <stst5821@gmail.com>
2021-08-17 20:00:29 +0900

    配列data,sort,map関数をindex.tsxに移動

--- src/pages/components/persons.tsx
+++ src/pages/components/persons.tsx
@@ -6,37 +6,19 @@ type members = {
   id: number;
 };
 
-const members = [
-  { name: "としみつ", tel: "090-0000-0000", id: 3 },
-  { name: "rika", tel: "090-0000-0010", id: 5 },
-  { name: "まさき", tel: "090-0110-0010", id: 8 },
-  { name: "yoshiaki", tel: "090-0010-0010", id: 1 },
-  { name: "いっち", tel: "090-0010-0011", id: 2 },
-];
-
-members.sort(function (a, b) {
-  if (a.id > b.id) {
-    return 1;
-  } else {
-    return -1;
-  }
-});
-
-export const Persons: FC<members> = () => {
+const Persons: FC<members> = (member) => {
   return (
     <>
       <ul>
-        {members.map((member) => (
-          <div key={member.id}>
-            <li>
-              名前:{member.name}
-              <br />
-              電話番号:{member.tel}
-              <br />
-              ID:{member.id}
-            </li>
-          </div>
-        ))}
+        <div key={member.id}>
+          <li>
+            名前:{member.name}
+            <br />
+            電話番号:{member.tel}
+            <br />
+            ID:{member.id}
+          </li>
+        </div>
       </ul>
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,22 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Persons from "../pages/components/persons";
 
+const members = [
+  { name: "としみつ", tel: "090-0000-0000", id: 3 },
+  { name: "rika", tel: "090-0000-0010", id: 5 },
+  { name: "まさき", tel: "090-0110-0010", id: 8 },
+  { name: "yoshiaki", tel: "090-0010-0010", id: 1 },
+  { name: "いっち", tel: "090-0010-0011", id: 2 },
+];
+
+members.sort(function (a, b) {
+  if (a.id > b.id) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -12,7 +28,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Typescript</h1>
-      <Persons />
+      {members.map((member) => (
+        <Persons
+          key={member.id}
+          name={member.name}
+          id={member.id}
+          tel={member.tel}
+        />
+      ))}
     </div>
   );
 };
